8dbc5d2e6a325331b438923856fe853d8677503c
RulerOfCakes <fragmentbombs@gmail.com>
2023-08-19 01:58:00 +0900

    feat: add axios & service layout

---
+++ backend/src/util/axios.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosError, isAxiosError } from "axios";
+
+export interface HttpError extends Record<string, any> {
+  message: string;
+  statusCode: number;
+}
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error): Promise<HttpError> => {
+    if (isAxiosError(error)) {
+      return Promise.reject({
+        ...error,
+        message: error.message,
+        statusCode: error.code,
+      });
+    }
+    const customError: HttpError = {
+      ...error,
+      message: error.response?.data?.message,
+      statusCode: error.response?.status,
+    };
+
+    return Promise.reject(customError);
+  }
+);
+
+export { axiosInstance };
