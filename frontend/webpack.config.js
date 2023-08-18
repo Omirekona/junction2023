ad302889cf80eb776fa5a2af2f35e57602d080c9
Asmir <asmir8717@gmail.com>
2023-08-19 03:31:55 +0900

    added initial react set up

---
+++ frontend/webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".jsx", ".js", ".css"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html")
+        })
+    ],
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "./build")
+    },
+    devServer: {
+        static: "./build",
+        port: 3001,
+        hot: true,
+        open: true,
+        historyApiFallback: true,
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000",
+                secure: false
+            }
+        }
+    },
+    watchOptions: {
+        ignored: [path.resolve(__dirname, "./node_modules"), path.resolve(__dirname, "./build")]
+    }
+}
