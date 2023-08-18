c8e035d851f68b6f77691088346d9c9a2981e499
Asmir <asmir8717@gmail.com>
2023-08-19 03:42:04 +0900

    Revert "changed the backend"
    This reverts commit 73a1982e7bf6a72caeeaaa5f081fbb329b7e8e70.

---
+++ backend/src/routes/constants/Paths.ts
@@ -0,0 +1,25 @@
+/**
+ * Express router paths go here.
+ */
+
+import { Immutable } from "@src/other/types";
+
+const Paths = {
+  Base: "/api",
+  Users: {
+    Base: "/users",
+    Get: "/all",
+    Add: "/add",
+    Update: "/update",
+    Delete: "/delete/:id",
+  },
+  Attractions: {
+    Base: "/attractions",
+    Get: "/all",
+  },
+};
+
+// **** Export **** //
+
+export type TPaths = Immutable<typeof Paths>;
+export default Paths as TPaths;
