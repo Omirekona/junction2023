731d0a316270938774e5ed955585ecf3460e92ef
Asmir <asmir8717@gmail.com>
2023-08-19 08:43:43 +0900

    making some changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import db from "./repos/db";
 
 // **** Run **** //
 const port = process.env.PORT || 3000;
+
+console.log("the port is: ", port);
 const service = server.listen(port);
 
 service.setTimeout(20000);
