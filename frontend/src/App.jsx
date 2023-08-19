7607829418f613c4708283a09d79afbcbdf5c280
edoszhan <yersultanofficial@gmail.com>
2023-08-19 20:15:25 +0900

    log in works and sends to maps for now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
+      <div className="App" style={{backgroundColor: 'beige'}}>
         <main>
           {/* Route Handling */}
           <Routes>
