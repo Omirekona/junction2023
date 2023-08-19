09f40a9d9fc7250aac3921e56778e01aab9bbb36
RulerOfCakes <fragmentbombs@gmail.com>
2023-08-20 00:23:07 +0900

    feat: add api descriptions & new mission table

--- backend/src/handlers/api.ts
+++ backend/src/handlers/api.ts
@@ -14,21 +14,30 @@ const routeRouter = Router();
 const userRouter = Router();
 const imageRouter = Router();
 
-// Get all attractions
+// Get an attraction's info
+// api/attraction?uc_seq=${uc_seq}&preference=${preference}
 attractionRouter.get(Paths.Attraction.Get, AttractionHandler.get);
 
-// Get route by preference
+// generate new route
 // api/route/new?preference=${preference}
 routeRouter.get(Paths.Route.GetNew, RouteHandler.getNew);
-// api/route?routeId=${routeId}
+
+// get previously generated route info by id
+// api/route?routeId=${routeId} -> one unique route
+// api/route?userId=${userId} -> all routes belonging to user
 routeRouter.get(Paths.Route.Get, RouteHandler.get);
 
-// User API
+// User API (WIP, need firebase integration)
+// api/user?userId=${userId} -> get user info
 userRouter.get("", UserHandler.get);
+// api/user -> create new user in db with firebase id
 userRouter.post("", UserHandler.create);
+// api/user/points -> add points to user
 userRouter.post(Paths.User.Points, UserHandler.addPoints);
 
 // Image API
+// api/image/compare?uc_seq=${uc_seq}&image=${image}&preference={preference}
+// compare the given `image` with the attraction's image, which is derived from UC_SEQ(unique id for attraction per preference) and preference
 imageRouter.get(Paths.Image.Compare, ImageHandler.compare);
 
 // Add the feature specific routers to api router
--- backend/src/repos/db.ts
+++ backend/src/repos/db.ts
@@ -13,6 +13,10 @@ db.serialize(() => {
   db.run(
     "CREATE TABLE route (id INTEGER PRIMARY KEY, name TEXT, user_id NUMBER, info TEXT, progress INTEGER)"
   );
+  // `route_id` and `progress` are used as foreign keys to indicate in which part of a route the mission belongs to
+  db.run(
+    "CREATE TABLE mission (id INTEGER PRIMARY KEY, name TEXT, route_id NUMBER, info TEXT, progress INTEGER level INTEGER is_complete INTEGER)"
+  )
 });
 
 export default db;
