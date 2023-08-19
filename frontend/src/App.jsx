db04dbd53eb15c90e71a13cda04a2d86d3d79737
Asmir <asmir8717@gmail.com>
2023-08-19 10:16:20 +0900

    testti9g

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     }, []);
 
     return (
-        <div>the response from server is: {hello} yay seeming works now lol wow</div>
+        <div> sdfsdf the response from server is: {hello} yay seeming works now lol wow</div>
     )
 }
 
