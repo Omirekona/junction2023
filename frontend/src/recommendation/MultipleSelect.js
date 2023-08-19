07a24ba508d255fef4c69e621d5beaf386e93790
edoszhan <yersultanofficial@gmail.com>
2023-08-20 06:56:44 +0900

    preference1 base done

--- frontend/src/recommendation/MultipleSelect.js
+++ frontend/src/recommendation/MultipleSelect.js
@@ -2,60 +2,173 @@ import React from "react";
 import '../App.css';
 import { Button, Checkbox } from '@mui/material';
 import { FormGroup, FormControlLabel } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 function MultipleSelect() {
-    return (
-    <div>What kind of trip do you prefer?
-      <div className="mb-3 rounded-xl">
-      <label style={{ padding: '5px', border: '1px solid gray', display: 'inline-block', borderRadius: '8px' }} className='mr-3 custom-checkbox'>
-    <input type="checkbox" onChange={() => { }} />
-    <span className="ml-2 mr-2 font-bold font text-slate-500">Family</span>
-</label>
 
-      <label>
-        <input type="checkbox" onChange={() => {}} className="rounded-full " />
-        <span className="ml-2 mr-2">Single</span>
-      </label>
-      <label>
-        <input type="checkbox" onChange={() => {}} className="rounded-full " />
-        <span className="ml-2 mr-2">Couple</span>
-      </label>
-      </div>
-
-      <div>
-      <label style={{padding: '3px'}} className='rounded br-2 border-black bg-red-500'>
-        <input type="checkbox" onChange={() => {}}/>
-        <span className="ml-2 mr-2" >Nature</span>
-      </label>
-      <label>
-        <input type="checkbox" onChange={() => {}} className="rounded-full " />
-        <span className="ml-2 mr-2">Festival</span>
-      </label>
-      <label>
-        <input type="checkbox" onChange={() => {}} className="rounded-full " />
-        <span className="ml-2 mr-2">Food</span>
-      </label>
-      </div>
-
-      <FormGroup>
-  <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
-  <FormControlLabel required control={<Checkbox />} label="Required" />
-  <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
-</FormGroup>
-
-
-
-      <div className="mt-8 mb-10">
-    <Button variant="contained" >
-      Next
-    </Button>
-      </div>
-
-    </div>
-      
-
-
-    );
+  const customStyle = {
+    padding: '1px', 
+    border: '1px solid gray', 
+    display: 'inline-block', 
+    borderRadius: '8px',
+    marginRight: '15px',
+    cursor: 'pointer',
+    marginLeft: '15px',
+    fontWeight: 'bold',
   }
-  
-  export default MultipleSelect;   
+
+  return (
+    <div>
+      <div className="mb-10 mt-5">
+      What kind of trip do you prefer?
+      </div>
+      
+      <div className="mb-3 rounded-xl">
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Family</span>}
+          labelPlacement="end"
+        />
+
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Single</span>}
+          labelPlacement="end"
+        />
+
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Couple</span>}
+          labelPlacement="end"
+        />
+      </div>
+
+      <div className="mb-3 rounded-xl">
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Nature</span>}
+          labelPlacement="end"
+        />
+
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Festival</span>}
+          labelPlacement="end"
+        />
+
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Food</span>}
+          labelPlacement="end"
+        />
+      </div>
+
+      <div className="mb-3 rounded-xl">
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Arts</span>}
+          labelPlacement="end"
+        />
+
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Film</span>}
+          labelPlacement="end"
+        />
+
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Shopping</span>}
+          labelPlacement="end"
+        />
+      </div>
+
+      <div className="mb-3 rounded-xl">
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Random</span>}
+          labelPlacement="end"
+        />
+
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Music</span>}
+          labelPlacement="end"
+        />
+
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Bike</span>}
+          labelPlacement="end"
+        />
+      </div>
+
+
+      <div className="mb-3 rounded-xl">
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Car</span>}
+          labelPlacement="end"
+        />
+
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Bus</span>}
+          labelPlacement="end"
+        />
+
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Train</span>}
+          labelPlacement="end"
+        />
+      </div>
+
+      <div className="mb-3 rounded-xl">
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Ship</span>}
+          labelPlacement="end"
+        />
+
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>City</span>}
+          labelPlacement="end"
+        />
+
+        <FormControlLabel
+          style={customStyle}
+          control={<Checkbox onChange={() => { }} />}
+          label={<span style={{fontWeight: 'bold', marginRight: '10px'}}>Countryside</span>}
+          labelPlacement="end"
+        />
+      </div>
+
+      <Button variant="contained" style={{borderRadius: '10px', marginBottom: '10px', marginTop: '5px'}}>
+        <Link to="/preference2" style={{textDecoration: 'none', color: 'black'}}>Next</Link>
+      </Button>
+
+      
+    </div>
+  );
+}
+
+export default MultipleSelect;
