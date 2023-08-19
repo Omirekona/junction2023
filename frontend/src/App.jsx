bb72ef5a27bd647c57c32742346217232d206ce4
edoszhan <yersultanofficial@gmail.com>
2023-08-19 14:54:27 +0900

    routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
-import {useEffect, useState} from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import MapsPage from './components/MapsPage';
+import MissionPage from './components/MissionPage';
+import LoginPage from './auth/Login';
+import './App.css';
+import Drawer from 'react-modern-drawer'
 
-import "./App.css";
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
 
-//dev
-const App = () => {
-    const [hello, setHello] = useState("");
-
-    useEffect(() => {
-        fetch("/api/hello").then(response => response.text()).then((finalResponse) => setHello(finalResponse))
-    }, []);
-
-    return (
-        <div> the responses from server is: {hello} yay seeming works now lol wow</div>
-    )
+  return (
+    <Router>
+      <div className="App">
+        <header>
+        <button onClick={toggleDrawer}>Menu</button>
+          <nav>
+            <Drawer open={isOpen} onClose={toggleDrawer} direction='left' className='drawer z-10' > 
+              <ul>
+                <li><Link to="/" onClick={toggleDrawer}>Home</Link></li>
+                <li><Link to="/login" onClick={toggleDrawer}>Log in</Link></li>
+                <li><Link to="/maps" onClick={toggleDrawer}>Maps</Link></li>
+                <li><Link to="/mission" onClick={toggleDrawer}>Mission</Link></li>
+              </ul>
+            </Drawer>
+          </nav>
+        </header>
+        
+        <main>
+          {/* Route Handling */}
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/maps" element={<MapsPage />}/>
+            <Route path="/mission" element={<MissionPage/>} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
